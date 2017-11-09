5957421be40a324220d707b9ac7817648d0b2cdf
mumayank <mumayank@gmail.com>
2017-11-09 16:13:34 +0530

    add Constants.java class

---
+++ app/src/main/java/mumayank/reactivenews/util/Constants.java
@@ -0,0 +1,11 @@
+package mumayank.reactivenews.util;
+
+/**
+ * Created by Mayank on 09-11-2017.
+ */
+
+public class Constants {
+    public static final long CACHE_VALID_DURATION = 10 * 60 * 1000; // in ms
+
+    public static final String LAST_DATA_STORED_IN_DB_TIME = "LAST_DATA_STORED_IN_DB_TIME";
+}
