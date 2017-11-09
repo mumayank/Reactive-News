b12a5d9abffc75d8bf15397475d526c7c5de9b2f
mumayank <mumayank@gmail.com>
2017-11-09 16:13:03 +0530

    add app class which extends Application

---
+++ app/src/main/java/mumayank/reactivenews/App.java
@@ -0,0 +1,16 @@
+package mumayank.reactivenews;
+
+import android.app.Application;
+
+/**
+ * Created by Mayank on 09-11-2017.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
